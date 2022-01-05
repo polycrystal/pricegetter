e0b79957d16ac8a591ac07e8d1d3db9858ae9082
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2022-01-05 00:35:31 -0600

    Update IAMMInfo.sol

--- contracts/libs/IAMMInfo.sol
+++ contracts/libs/IAMMInfo.sol
@@ -13,7 +13,9 @@ import "./IUniRouter.sol";
 import "./IUniPair.sol";
 
 // calculates the CREATE2 address for a pair without making any external calls
-function pairFor(address tokenA, address tokenB, address factory, bytes32 initcodehash) pure returns (address pair) {
+function pairFor(address tokenA, address tokenB, address factory, bytes32 initcodehash) view returns (address pair) {
+    if (initcodehash == 0) return IUniFactory(factory).getPair(tokenA, tokenB);
+
     (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
     pair = address(uint160(uint(keccak256(abi.encodePacked(
             hex'ff',
